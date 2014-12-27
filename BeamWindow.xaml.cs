db74f972f1901b2d4d22df4fa9bdfe21d0aa8d97
Mystika <devbirdlee@gmail.com>
2014-12-27 16:20:54 +0900

    design PIN auth section and add animation

--- BeamWindow.xaml.cs
+++ BeamWindow.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Beam.oAuth;
+using System.Web;
+using System.Windows.Media.Animation;
 
 namespace Beam
 {
@@ -19,9 +22,47 @@ namespace Beam
     /// </summary>
     public partial class BeamWindow : Window
     {
+        Twitter t = new Twitter();
+        string token;
         public BeamWindow()
         {
             InitializeComponent();
+            tbSlideBack.MouseDown += delegate { slideGrid(2); };
+            tbSlideBack.TouchDown += delegate { slideGrid(2); };
+        }
+
+        private void btSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            slideGrid(1);
+            return;
+            Uri uri = new Uri(t.AuthorizationLinkGet());
+            System.Diagnostics.Process.Start(uri.ToString());
+            token = HttpUtility.ParseQueryString(uri.Query)["oauth_token"];
+            
+            this.IsEnabled = false;
+            
+        }
+
+        private void slideGrid(int direction)
+        {
+            ThicknessAnimation da;
+            ThicknessAnimation da2;
+            //1 Left, 2 Right
+            switch (direction)
+            {
+                case 1:
+                    da = new ThicknessAnimation(new Thickness(0),new Duration(TimeSpan.FromMilliseconds(200)));
+                    da2 = new ThicknessAnimation(new Thickness(-295,0,0,0), new Duration(TimeSpan.FromMilliseconds(200)));
+                    break;
+                case 2:
+                default:
+                    da = new ThicknessAnimation(new Thickness(295,0,0,0), new Duration(TimeSpan.FromMilliseconds(200)));
+                    da2 = new ThicknessAnimation(new Thickness(0), new Duration(TimeSpan.FromMilliseconds(200)));
+                    break;
+            }
+         
+            grdSignIn.BeginAnimation(MarginProperty, da2);
+            grdPIN.BeginAnimation(MarginProperty, da);
         }
     }
 }
